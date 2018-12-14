44c7479c9488191169bf944db3d8ee59fe579767
wyz <yuzhongwang@harmonycloud.cn>
2018-12-14 10:58:23 +0800

    add spring-boot module!

--- dispatchingExecution/src/main/java/cn/harmonycloud/interfaces/DispatchInterfaces.java
+++ dispatchingExecution/src/main/java/cn/harmonycloud/interfaces/DispatchInterfaces.java
@@ -4,6 +4,8 @@ import cn.harmonycloud.implementation.CreatePodCallable;
 import cn.harmonycloud.implementation.DeletePodCallable;
 import cn.harmonycloud.implementation.PodsImplementation;
 import cn.harmonycloud.utils.ThreadPoolUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -11,9 +13,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
 //springboot或者springmvc等框架
+@RestController
+@RequestMapping("/dispatching")
 public class DispatchInterfaces {
+
+
     private ExecutorService THREAD_POOL = ThreadPoolUtils.getInstance();
     //createPod
+    @RequestMapping("/createPod")
     public boolean createPod(String ruleJSON) throws ExecutionException, InterruptedException {
         CreatePodCallable createPodCallable = new CreatePodCallable(ruleJSON);
         Future<Boolean> result = THREAD_POOL.submit(createPodCallable);
@@ -21,6 +28,7 @@ public class DispatchInterfaces {
     }
 
     //deletePod
+    @RequestMapping("/deletePod")
     public boolean deletePod(String json) throws ExecutionException, InterruptedException {
         DeletePodCallable deletePodCallable = new DeletePodCallable(json);
         Future<Boolean> result = THREAD_POOL.submit(deletePodCallable);
