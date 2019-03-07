97e82af311191bfe2bb47aacada6bfa22a90a09d
DellonShao <14301102@bjtu.edu.cn>
2019-03-07 17:46:58 +0800

    删除一些文本

--- data-center/src/main/java/cn/harmonycloud/datacenter/controller/ServiceDataController.java
+++ data-center/src/main/java/cn/harmonycloud/datacenter/controller/ServiceDataController.java
@@ -5,7 +5,6 @@ import cn.harmonycloud.datacenter.entity.es.ServiceData;
 import cn.harmonycloud.datacenter.service.INodeDataService;
 import cn.harmonycloud.datacenter.service.IServiceDataService;
 import com.google.common.collect.Lists;
-import com.sun.xml.internal.ws.api.server.SDDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
