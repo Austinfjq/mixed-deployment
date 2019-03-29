cbde05de3b658298ebaef7132ddd2a242138f2ec
DellonShao <14301102@bjtu.edu.cn>
2019-03-29 17:07:50 +0800

    move /resources/application.properties to /data-center directory

--- data-center/src/main/java/cn/harmonycloud/datacenter/DataCenterApplication.java
+++ data-center/src/main/java/cn/harmonycloud/datacenter/DataCenterApplication.java
@@ -1,14 +1,26 @@
 package cn.harmonycloud.datacenter;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.util.Properties;
 
 @SpringBootApplication
 public class DataCenterApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DataCenterApplication.class, args);
-    }
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+//        InputStream in = DataCenterApplication.class.getClassLoader().getResourceAsStream("application.properties");
+        String path = System.getProperty("user.dir")+File.separator+"application.properties";
+        System.out.println(path);
+        InputStream in = new FileInputStream(new File(path));
+        properties.load(in);
 
+        SpringApplication app = new SpringApplication(DataCenterApplication.class);
+        app.setDefaultProperties(properties);
+        app.run(args);
+    }
 }
 
