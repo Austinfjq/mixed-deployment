e7de7cd788e6f2aaa5a5b5595caf91810128f5f4
DellonShao <14301102@bjtu.edu.cn>
2019-03-31 16:51:10 +0800

    修改配置文件路径代码

--- data-center/src/main/java/cn/harmonycloud/datacenter/DataCenterApplication.java
+++ data-center/src/main/java/cn/harmonycloud/datacenter/DataCenterApplication.java
@@ -12,12 +12,8 @@ public class DataCenterApplication {
 
     public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
-//        InputStream in = DataCenterApplication.class.getClassLoader().getResourceAsStream("application.properties");
-        String path = System.getProperty("user.dir")+File.separator+"application.properties";
-        System.out.println(path);
-        InputStream in = new FileInputStream(new File(path));
+        InputStream in = DataCenterApplication.class.getClassLoader().getResourceAsStream("application.properties");
         properties.load(in);
-
         SpringApplication app = new SpringApplication(DataCenterApplication.class);
         app.setDefaultProperties(properties);
         app.run(args);
