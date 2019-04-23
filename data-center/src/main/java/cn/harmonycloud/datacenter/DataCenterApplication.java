f9950937db86edfe7d35e55ff695e37f1db20797
DellonShao <14301102@bjtu.edu.cn>
2019-04-23 17:18:17 +0800

    修改management接口，返回最新的数据； 简单配置Druid连接池

--- data-center/src/main/java/cn/harmonycloud/datacenter/DataCenterApplication.java
+++ data-center/src/main/java/cn/harmonycloud/datacenter/DataCenterApplication.java
@@ -1,12 +1,10 @@
 package cn.harmonycloud.datacenter;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 @SpringBootApplication
@@ -15,19 +13,17 @@ public class DataCenterApplication {
     public static void main(String[] args) throws IOException {
 //        Properties properties = new Properties();
 //        InputStream in = null;
-//        //String path = "./usr/local/data-center/application.properties";
-//        //String path = "/root/application/application.properties";
+////        String path = "./usr/local/data-center/application.properties";
 //        String path = "./data-center/application.properties";
-        //String path = "E:\\mixed-deployment\\data-center\\application.properties";
 //        if(args.length != 0){//args[0]-->配置文件地址
-//            in = new FileInputStream(new File(path));
+//            in = new FileInputStream(new File(args[0]));
 //        }else {
 //            //in = DataCenterApplication.class.getClassLoader().getResourceAsStream("application.properties");
 //            in = new FileInputStream(new File(path));
 //        }
 //        properties.load(in);
         SpringApplication app = new SpringApplication(DataCenterApplication.class);
-//      app.setDefaultProperties(properties);
+//        app.setDefaultProperties(properties);
         app.run(args);
     }
 }
