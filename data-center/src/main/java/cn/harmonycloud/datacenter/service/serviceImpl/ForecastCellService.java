3d212d88094f33ed97c54c1bae0cd1fbbc7a383f
DellonShao <14301102@bjtu.edu.cn>
2019-04-25 13:51:02 +0800

    添加负载预测模块的三个接口： (1)	获取某一个ForecastCell； (2)	保存ForecastCell的预测模型； (3)	保存ForecastCell完成预测的未来最远时间点。

--- data-center/src/main/java/cn/harmonycloud/datacenter/service/serviceImpl/ForecastCellService.java
+++ data-center/src/main/java/cn/harmonycloud/datacenter/service/serviceImpl/ForecastCellService.java
@@ -23,4 +23,30 @@ public class ForecastCellService implements IForecastCellService {
     public List<ForecastCell> getAllForecastCells() {
         return forecastCellMapper.getAllForecastCells();
     }
+
+    @Override
+    public ForecastCell getOneForecastCellByIndex(String cellId, int type, String forecastingIndex) {
+        return forecastCellMapper.getOneForecastCellByIndex(cellId,type,forecastingIndex);
+    }
+
+    @Override
+    public int saveForecastingModel(String cellId, int type, String forecastingIndex, String forecastingModel, String modelParams) {
+        ForecastCell forecastCell = getOneForecastCellByIndex(cellId,type,forecastingIndex);
+        if(forecastCell == null){
+            return forecastCellMapper.insertForecastingModel(cellId,type,forecastingIndex,forecastingModel,modelParams);
+        }else{
+            return forecastCellMapper.updateForecastingModel(cellId,type,forecastingIndex,forecastingModel,modelParams);
+        }
+    }
+
+    @Override
+    public int saveForecastingEndTime(String cellId, int type, String forecastingIndex, String forecastingEndTime) {
+        ForecastCell forecastCell = getOneForecastCellByIndex(cellId,type,forecastingIndex);
+        if(forecastCell == null){
+            return forecastCellMapper.insertForecastingEndTime(cellId,type,forecastingIndex,forecastingEndTime);
+        }else{
+            return forecastCellMapper.updateForecastingEndTime(cellId,type,forecastingIndex,forecastingEndTime);
+        }
+    }
+
 }
