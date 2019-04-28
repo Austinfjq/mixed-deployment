4580b52ed1c4e74d427bf6fca859918e49b63cfe
zhouhongcen <zhc940827git>
2019-04-28 16:03:57 +0800

    接口代码修改

--- data-center/src/main/java/cn/harmonycloud/datacenter/mapper/ServiceMapper.java
+++ data-center/src/main/java/cn/harmonycloud/datacenter/mapper/ServiceMapper.java
@@ -19,4 +19,6 @@ public interface ServiceMapper {
     public Service findServiceByNamespaceAndServiceName(String namespace, String serviceName);
     @Select("SELECT * FROM service WHERE cluster_ip=#{clusterIp}")
     public List<Service> findServiceByClusterIp(String clusterIp);
+    @Select("SELECT pod_instances FROM serviceLoad_mapping_podInstances,service WHERE cluster_ip=#{clusterIp} AND namespace=#{namespace} AND service_name=#{serviceName} AND serviceLoad_mapping_podInstances.service_id=service.service_id")
+    public int findPodNumsByService(String clusterIp,String namespace, String serviceName);
 }
