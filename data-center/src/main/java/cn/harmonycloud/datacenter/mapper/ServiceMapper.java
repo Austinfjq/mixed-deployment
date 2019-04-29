d535bc3d77d3e1ef8299f5404171f61099e3e94f
DellonShao <14301102@bjtu.edu.cn>
2019-04-29 09:44:21 +0800

    修改/service/podInstances接口的bug.

--- data-center/src/main/java/cn/harmonycloud/datacenter/mapper/ServiceMapper.java
+++ data-center/src/main/java/cn/harmonycloud/datacenter/mapper/ServiceMapper.java
@@ -19,6 +19,6 @@ public interface ServiceMapper {
     public Service findServiceByNamespaceAndServiceName(String namespace, String serviceName);
     @Select("SELECT * FROM service WHERE cluster_ip=#{clusterIp}")
     public List<Service> findServiceByClusterIp(String clusterIp);
-    @Select("SELECT pod_instances FROM serviceLoad_mapping_podInstances,service WHERE cluster_ip=#{clusterIp} AND namespace=#{namespace} AND service_name=#{serviceName} AND serviceLoad_mapping_podInstances.service_id=service.service_id")
+    @Select("SELECT pod_instances FROM serviceload_mapping_podinstances,service WHERE cluster_ip=#{clusterIp} AND namespace=#{namespace} AND service_name=#{serviceName} AND serviceload_mapping_podinstances.service_id=service.service_id")
     public int findPodNumsByService(String clusterIp,String namespace, String serviceName);
 }
